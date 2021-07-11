c9932868deda4cef2006ea3be235d0b370394856
keitatofu <roidthebeast@gmail.com>
2021-07-11 18:10:05 +0300

    revamping portfolio from here

--- js/theme-change.js
+++
@@ -1,9 +0,0 @@
-function switchSheet() {
-    let theme = document.getElementById("theme");
-
-    if (theme.getAttribute("href") == "css/themes/light-mode.css") {
-        theme.href = "css/themes/dark-mode.css";
-    } else {
-        theme.href = "css/themes/light-mode.css";
-    }
-}
