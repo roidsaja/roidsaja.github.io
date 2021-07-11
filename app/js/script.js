bbc5fe967b7153e356a17f90d57d959245f0af8e
keitatofu <roidthebeast@gmail.com>
2021-07-11 22:56:12 +0300

    contact, footer added

--- app/js/script.js
+++ app/js/script.js
@@ -3,127 +3,149 @@
  * @param {number} ms Number of milliseconds to wait
  * @returns Promise
  */
- async function sleep(ms) {
+async function sleep(ms) {
     return new Promise((r) => setTimeout(r, ms));
-  }
-  
-  /**
-   * Remove a class from an element
-   * @param {string} selector CSS selector for the element
-   * @param {string} className A single class name
-   */
-  function removeClass(selector, className) {
+}
+
+/**
+ * Remove a class from an element
+ * @param {string} selector CSS selector for the element
+ * @param {string} className A single class name
+ */
+function removeClass(selector, className) {
     document.querySelector(selector).classList.remove(className);
-  }
-  
-  /** ScrollReveal (https://scrollrevealjs.org) */
-  
-  const popUp = {
+}
+
+/** ScrollReveal (https://scrollrevealjs.org) */
+
+const popUp = {
     delay: 150,
     scale: 0.92,
     viewFactor: 0.25, // Reveal when 25% of elements are within viewport
-  };
-  
-  const popUpItems = [".about", ".work", ".project__header", ".project__item", ".contact"];
-  popUpItems.forEach((item) => {
+};
+
+const popUpItems = [".about", ".experiences", ".project__header", ".project__item", ".contact"];
+popUpItems.forEach((item) => {
     ScrollReveal().reveal(item, popUp);
-  });
-  
-  /** TypeIt.js (https://typeitjs.com/docs) */
-  
-  const heroTitle2 = new TypeIt("#hero__title2", {
+});
+
+/** TypeIt.js (https://typeitjs.com/docs) */
+
+const heroTitle2 = new TypeIt("#hero__title2", {
     speed: 27,
     startDelay: 350,
     cursorSpeed: 2000,
     afterComplete: async (step, instance) => {
-      await sleep(150);
-      instance.destroy();
-      ScrollReveal().reveal(".hero__content", { delay: 100, distance: "10px", origin: "left" });
+        await sleep(150);
+        instance.destroy();
+        ScrollReveal().reveal(".hero__content", { delay: 100, distance: "10px", origin: "left" });
     },
-  });
-  
-  new TypeIt("#hero__title1", {
+});
+
+new TypeIt("#hero__title1", {
     strings: 'Hi, I\'m Roid!',
     html: true,
     speed: 25,
     startDelay: 500,
     afterComplete: async (step, instance) => {
-      await sleep(450);
-      instance.destroy(); // Remove first title's cursor
-      removeClass("#hero__chevron2", "invisible");
-      removeClass("#hero__title2", "invisible");
-      heroTitle2.go(); // Begin "typing" second title
+        await sleep(450);
+        instance.destroy(); // Remove first title's cursor
+        removeClass("#hero__chevron2", "invisible");
+        removeClass("#hero__title2", "invisible");
+        heroTitle2.go(); // Begin "typing" second title
     },
-  }).go();
-  
-  /** Navbar & Links handling */
-  
-  const header = document.querySelector(".header");
-  const headerOverlay = document.querySelector(".header__overlay");
-  const hamburger = document.querySelector("#hamburger");
-  const navLogo = document.querySelector(".nav__logo");
-  const navLinks = document.querySelectorAll(".nav__links > li > a");
-  const navLinksMobile = document.querySelectorAll(".nav__mobile_links > li > a");
-  const fadeElements = document.querySelectorAll(".has-fade");
-  
-  function openMobileMenu() {
+}).go();
+
+/** Navbar & Links handling */
+
+const header = document.querySelector(".header");
+const headerOverlay = document.querySelector(".header__overlay");
+const hamburger = document.querySelector("#hamburger");
+const navLogo = document.querySelector(".nav__logo");
+const navLinks = document.querySelectorAll(".nav__links > li > a");
+const navLinksMobile = document.querySelectorAll(".nav__mobile_links > li > a");
+const fadeElements = document.querySelectorAll(".has-fade");
+
+function openMobileMenu() {
     header.classList.add("open");
     fadeElements.forEach((item) => {
-      item.classList.add("fade-in");
-      item.classList.remove("fade-out");
+        item.classList.add("fade-in");
+        item.classList.remove("fade-out");
     });
-  }
-  
-  function closeMobileMenu() {
+}
+
+function closeMobileMenu() {
     header.classList.remove("open");
     fadeElements.forEach((item) => {
-      item.classList.remove("fade-in");
-      item.classList.add("fade-out");
+        item.classList.remove("fade-in");
+        item.classList.add("fade-out");
     });
-  }
-  
-  // Toggling hamburger icon in navbar
-  hamburger.addEventListener("click", () => {
+}
+
+// Toggling hamburger icon in navbar
+hamburger.addEventListener("click", () => {
     header.classList.contains("open") ? closeMobileMenu() : openMobileMenu();
-  });
-  
-  // Close mobile menu after clicking certain elements
-  const closeMenuElements = [...navLinksMobile, headerOverlay];
-  closeMenuElements.forEach((element) => {
+});
+
+// Close mobile menu after clicking certain elements
+const closeMenuElements = [...navLinksMobile, headerOverlay];
+closeMenuElements.forEach((element) => {
     element.addEventListener("click", function () {
-      closeMobileMenu();
+        closeMobileMenu();
     });
-  });
-  
-  // Remove focus after clicking for buttons & anchor links
-  const focusElements = [...navLinks, hamburger, navLogo];
-  focusElements.forEach((element) => {
+});
+
+// Remove focus after clicking for buttons & anchor links
+const focusElements = [...navLinks, hamburger, navLogo];
+focusElements.forEach((element) => {
     element.addEventListener("click", function () {
-      element.blur();
+        element.blur();
     });
-  });
-  
-  /** Get in Touch - Copy email to clipboard */
-  
-  const copyEmailBtn = document.querySelector(".contact__copy_email");
-  const successInfoBox = document.querySelector(".contact__info_box.copy-success");
-  const failInfoBox = document.querySelector(".contact__info_box.copy-fail");
-  
-  // Callback function after the popup box has finished animating
-  const animEndCallback = (e) => {
+});
+
+/** Work Experience tab view */
+
+const tabs = document.querySelectorAll(".experiences__tabs > .tab");
+
+tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => handleTabClick(e));
+});
+
+function handleTabClick(e) {
+    const target = e.target; // Clicked tab button
+    const tabNum = target.dataset.tab; // Returns "data-tab" value
+    if (!tabNum) return;
+
+    const activeTab = document.querySelector(".experiences__tabs .active");
+    const activeContent = document.querySelector(".experiences__content .visible");
+    const currentContent = document.querySelector(`.experiences__content_section[data-tab='${tabNum}']`);
+
+    activeTab.classList.remove("active");
+    target.classList.add("active");
+    activeContent.classList.remove("visible");
+    currentContent.classList.add("visible");
+}
+
+/** Get in Touch - Copy email to clipboard */
+
+const copyEmailBtn = document.querySelector(".contact__copy_email");
+const successInfoBox = document.querySelector(".contact__info_box.copy-success");
+const failInfoBox = document.querySelector(".contact__info_box.copy-fail");
+
+// Callback function after the popup box has finished animating
+const animEndCallback = (e) => {
     e.target.classList.remove("fade-in-out");
-  };
-  
-  copyEmailBtn.addEventListener("click", (e) => {
+};
+
+copyEmailBtn.addEventListener("click", (e) => {
     navigator.clipboard.writeText("roid0520@gmail.com").then(
-      () => {
-        successInfoBox.classList.add("fade-in-out");
-        successInfoBox.addEventListener("animationend", animEndCallback);
-      },
-      (err) => {
-        failInfoBox.classList.add("fade-in-out");
-        failInfoBox.addEventListener("animationend", animEndCallback);
-      }
+        () => {
+            successInfoBox.classList.add("fade-in-out");
+            successInfoBox.addEventListener("animationend", animEndCallback);
+        },
+        (err) => {
+            failInfoBox.classList.add("fade-in-out");
+            failInfoBox.addEventListener("animationend", animEndCallback);
+        }
     );
-  });
-  
+});
